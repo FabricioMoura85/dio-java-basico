c7a17fc9289797dcb4f2b9eae15b68397a6e8e5f
Fabricio Moura <fabricio_michell@yahoo.com.br>
2023-10-16 22:42:56 -0400

    Atualizado Pastas

--- controlebanco/src/controlebanco.java
+++ controlebanco/src/controlebanco.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class controlebanco {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Integer Numero = 0;
+        String Agencia;
+        String NomeCliente;
+        float Saldo;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Por Favor, digite o numero da agencia");
+        Agencia = sc.next();
+         System.out.println("Por Favor, digite o seu nome");
+        NomeCliente = sc.next();
+         System.out.println("Por Favor, digite o seu saldo");
+        Saldo = sc.nextFloat();
+        Numero++;
+       
+    System.out.println("Ola "+ NomeCliente +", Obrigado por criar uma conta em nosso banco. sua agencia é "+Agencia+",conta " +Numero+",e seu saldo "+Saldo+" já está disponivél para Saque" );
+ sc.close();
     }
 }
